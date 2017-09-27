20de49b03968dc1431f759c926e56f36a05ec58d
Mike <mthomsonbrown@gmail.com>
2017-09-26 19:36:46 -0700

    Added token authentication to an api route

--- server/routes/api.js
+++ server/routes/api.js
@@ -15,39 +15,69 @@ const jwt = require('jsonwebtoken');
 /** TODO: write a meaningful comment... */
 mongoose.Promise = global.Promise;
 
+verifyAuthToken = function(req, res, next) {
+  // check header or url parameters or post parameters for token
+  var token = req.body.token || req.headers['x-access-token'];
+
+  // decode token
+  if (token) {
+    // verifies secret and checks exp
+    jwt.verify(token, secret, function(err, decoded) {
+      if (err) {
+        return res.json({
+          success: false,
+          message: 'Failed to authenticate token.'
+        });
+      } else {
+
+        // if everything is good, save to request for use in other routes
+        req.decoded = decoded;
+        next();
+      }
+    });
+
+  } else {
+
+    // if there is no token
+    // return an error
+    return res.status(403).send({
+      success: false,
+      message: 'No token provided.'
+    });
+  }
+};
+
 mongoose.connect(db, function(err) {
   if (err) {
     console.error('Error! ' + err);
   }
 });
 
-router.get('/daily_entries', function(req, res) {
-    console.log('Get request for daily entries.');
-    DailyEntry.find({})
+router.get('/daily_entries', verifyAuthToken, function(req, res) {
+  console.log('Get request for daily entries.');
+  DailyEntry.find({})
     .exec(function(err, daily_entries) {
       if (err) {
         console.error('Error retrieving daily entries: ' + err);
-      }
-      else {
+      } else {
         res.json(daily_entries);
       }
     });
 });
 
 router.post('/user', function(req, res) {
-    var user = new User();
-    user.name = req.body.name;
-    user.password = req.body.password;
-    user.password_confirmation = req.body.password_confirmation;
-    console.log('User is ' + req.body.name);
-    user.save(function(err, user) {
-      if (err) {
-        console.log('Error saving user: ' + err);
-      }
-      else {
-        res.json(user);
-      }
-    });
+  var user = new User();
+  user.name = req.body.name;
+  user.password = req.body.password;
+  user.password_confirmation = req.body.password_confirmation;
+  console.log('User is ' + req.body.name);
+  user.save(function(err, user) {
+    if (err) {
+      console.log('Error saving user: ' + err);
+    } else {
+      res.json(user);
+    }
+  });
 });
 
 router.post('/authenticate', function(req, res) {
@@ -58,20 +88,24 @@ router.post('/authenticate', function(req, res) {
 
     if (!user) {
       res.json({
-        success: false, message: 'Authentication failed. User not found.'
+        success: false,
+        message: 'Authentication failed. User not found.'
       });
     } else if (user) {
 
       // check if password matches
       if (user.password != req.body.password) {
         res.json({
-          success: false, message: 'Authentication failed. Wrong password.'
+          success: false,
+          message: 'Authentication failed. Wrong password.'
         });
       } else {
 
         // if user is found and password is right
         // create a token
-        var token = jwt.sign({user}, secret, {
+        var token = jwt.sign({
+          user
+        }, secret, {
           expiresIn: 60 * 60 * 24 // expires in 24 hours
         });
 
