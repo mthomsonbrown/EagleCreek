89f577b899b4324a9b1b507035a73f3821bb963f
Mike <mthomsonbrown@gmail.com>
2017-10-09 21:44:32 -0700

    Rooted out stray instances of @angular/http in favor of the newer @angular/common/httpclient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpModule } from '@angular/http';
 import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
@@ -30,7 +29,6 @@ import {
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpModule,
     FormsModule,
     HttpClientModule,
     BrowserAnimationsModule,
--- src/app/daily-entry.service.ts
+++ src/app/daily-entry.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-// import { Http, Response, Headers } from '@angular/http';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
 
