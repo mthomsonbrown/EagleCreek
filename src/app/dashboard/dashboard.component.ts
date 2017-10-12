021e34afbfdc25850d2511819d29d5c0342eedb3
Mike <mthomsonbrown@gmail.com>
2017-10-11 19:45:27 -0700

    Fixed bug where error content was showing briefly before success content

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,10 +1,12 @@
 /**
- * @fileoverview The dashboard should be a high level overview for authenticated users.
+ * @fileoverview The dashboard should be a high level overview for
+ * authenticated users.
  */
 
 import { Component, OnInit } from '@angular/core';
 import { DailyEntry } from './../daily-entry';
 import { DailyEntryService } from './../daily-entry.service';
+
 
 @Component({
   selector: 'app-dashboard',
@@ -16,14 +18,25 @@ export class DashboardComponent implements OnInit {
   date = new Date();
 
   daily_entry: DailyEntry;
+  currentUser: Boolean;
 
-  constructor(private daily_entry_service: DailyEntryService) { }
+  constructor(private daily_entry_service: DailyEntryService) {
+    this.currentUser = this.hasCurrentUser();
+  }
 
   ngOnInit() {
     this.daily_entry_service.get_daily_entry()
     .subscribe(entry => { this.daily_entry = entry[0]; });
   }
 
+  /**
+   * hasCurrentUser - Checks if there's a currentUser in localStorage.
+   *
+   * @return {Boolean}  True if there's a currentUser, false if not.
+   */
+  hasCurrentUser() {
+    return localStorage.getItem('currentUser') !== null;
+  }
 
   /**
    * onHi - Test function.  This should populate the div if the
